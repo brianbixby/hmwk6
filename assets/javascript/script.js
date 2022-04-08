c2366c46371e32fafe2c223ff78c0c5f66fa4706
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-08 05:06:55 -0700

    all data is displayed on the page needs a lot of styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,54 @@
 
 var searchFormEl = document.querySelector("#searchForm");
 
-function renderForecast(forecast) {
 
+var convertKtoF = k => parseFloat(Number(k) * 9 / 5 - 459.67).toFixed(2);
+
+function renderForecast(data) {
+    var cityDateTodayEl = document.querySelector("#cityDateToday");
+    var iconTodayEl = document.querySelector("#iconToday");
+    var tempTodayEl = document.querySelector("#tempToday");
+    var windTodayEl = document.querySelector("#windToday");
+    var humidityTodayEl = document.querySelector("#humidityToday");
+    var uviTodayEl = document.querySelector("#uviToday");
+
+    cityDateTodayEl.textContent = `${data.forecast[0][1]} ${moment(data.forecast[0][0]).format("M/D/YYYY")}`;
+    iconTodayEl.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[0][2]}.png`);
+    tempTodayEl.textContent = `Temp: ${convertKtoF(data.forecast[0][3])} °F`;
+    windTodayEl.textContent = `Wind: ${data.forecast[0][4]} MPH`;
+    humidityTodayEl.textContent = `Humidity: ${data.forecast[0][5]} %`;
+    uviTodayEl.textContent = `UV Index: ${data.forecast[0][6]}`;
+
+    var forecast5DayEl = document.querySelector("#forecast5Day");
+    forecast5DayEl.innerHTML = '';
+    for (let i = 1; i < data.forecast.length; i++) {
+        let div = document.createElement("div");
+        let h6 = document.createElement("h6");
+        let img = document.createElement("img");
+        let p1 = document.createElement("p1");
+        let p2 = document.createElement("p2");
+        let p3 = document.createElement("p3");
+
+        div.setAttribute("class", "card");
+        img.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[i][1]}.png`);
+        img.setAttribute("alt", "forecast icon");
+
+        h6.textContent = `${moment(data.forecast[i][0]).format("M/D/YYYY")}`;
+        p1.textContent = `Temp: ${convertKtoF(data.forecast[i][2])} °F`;
+        p2.textContent = `Wind: ${data.forecast[i][3]} MPH`;
+        p3.textContent = `Humidity: ${data.forecast[i][4]} %`;
+
+        div.appendChild(h6);
+        div.appendChild(img);
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        forecast5DayEl.appendChild(div);
+    }
 }
 
 function storeForecast(forecast) {
-    localStorage.setItem(`forecast-${forecast.city}`, JSON.stringify(forecast));
+    localStorage.setItem(`forecast-${forecast.city.toLowerCase()}`, JSON.stringify(forecast));
 }
 
 function forecastFetch(city) {
@@ -53,25 +95,20 @@ function forecastFetch(city) {
 function handleSubmit(event) {
     event.preventDefault();
 
-    var city = document.querySelector("#searchInput").value.trim();
+    var city = document.querySelector("#searchInput").value.trim().toLowerCase();
     if (!city) {
         // to do display error message
     } else {
         var storedForecast = JSON.parse(localStorage.getItem(`forecast-${city}`));
         if (storedForecast) {
             if (moment(storedForecast.date).isSame(new Date(), 'day')) {
-                console.log("handleSubmit if beg");
-                storeForecast(storedForecast);
                 renderForecast(storedForecast);
-                console.log("handleSubmit if end");
                 return;
             } else {
                 localStorage.removeItem(`forecast-${city}`);
             }
         }
-        console.log("handleSubmit else beg");
         forecastFetch(city);
-        console.log("handleSubmit else end");
     }
 }
 
