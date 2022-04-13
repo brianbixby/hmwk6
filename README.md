5e76562b448d83da48a6beb72e03d4ab29ec3c55
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-12 19:38:33 -0700

    added style and read me

--- README.md
+++ README.md
@@ -1,100 +1,27 @@
-# 06 Server-Side APIs: Weather Dashboard
+# HMWK6 - (06 Server-Side APIs: Weather Dashboard)
 
-## Your Task
+## Description
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
-
-Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
-
-## User Story
-
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
-
-## Acceptance Criteria
-
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+This is a weather dashboard that runs in the browser and features dynamically updated HTML and CSS. I utilized Moment.js in working with local storage in determining if a new API request was necessary. This web app also has a clean and polished, responsive user interface that adapts to multiple screen sizes.
 
 ## Mock-Up
 
-The following image shows the web application's appearance and functionality:
+The following image shows the web application's appearance:
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+![A screen shot of the calendar with a few past events.](./assets/images/hmwk6-screenshot.png)
 
-## Grading Requirements
+## Link
 
-> **Note**: If a homework assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+[Live site](https://brianbixby.github.io/hmwk6/)
 
-This homework is graded based on the following criteria: 
+## Technology
 
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Github](https://github.com/brianbixby/hmwk6)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/javascript)
+- [Local Storage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
+- [Moment.js](https://momentjs.com/)
+- [OpenWeather One Call API](https://openweathermap.org/api/one-call-api)
+- [Fetch](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+- [Bootstrap](https://getbootstrap.com/)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,186 +7,176 @@ var searches = [];
 var convertKtoF = k => parseFloat(Number(k) * 9 / 5 - 459.67).toFixed(2);
 
 function renderForecast(data) {
-    var forecastEl = document.querySelector("#forecast");
-    forecastEl.innerHTML = "";
+	var forecastEl = document.querySelector("#forecast");
+	forecastEl.innerHTML = "";
 
-    var h3El = document.createElement("h3");
-    var imgEl = document.createElement("img");
-    var p1El = document.createElement("p");
-    var p2El = document.createElement("p");
-    var p3El = document.createElement("p");
-    var p4El = document.createElement("p");
-    var spanEl = document.createElement("span");
+	var h2El = document.createElement("h2");
+	var imgEl = document.createElement("img");
+	var p1El = document.createElement("p");
+	var p2El = document.createElement("p");
+	var p3El = document.createElement("p");
+	var p4El = document.createElement("p");
+	var spanEl = document.createElement("span");
 
-    h3El.textContent = `${data.forecast[0][1]} ${moment(data.forecast[0][0]).format("M/D/YYYY")}`;
-    imgEl.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[0][2]}.png`);
-    imgEl.setAttribute("alt", "forecast icon");
-    p1El.textContent = `Temp: ${convertKtoF(data.forecast[0][3])} °F`;
-    p2El.textContent = `Wind: ${data.forecast[0][4]} MPH`;
-    p3El.textContent = `Humidity: ${data.forecast[0][5]} %`;
-    p4El.textContent = "UV Index: ";
-    spanEl.textContent = data.forecast[0][6];
+	h2El.textContent = `${data.forecast[0][1]} ${moment(data.forecast[0][0]).format("M/D/YYYY")}`;
+	imgEl.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[0][2]}.png`);
+	imgEl.setAttribute("alt", "forecast icon");
+	p1El.textContent = `Temp: ${convertKtoF(data.forecast[0][3])} °F`;
+	p2El.textContent = `Wind: ${data.forecast[0][4]} MPH`;
+	p3El.textContent = `Humidity: ${data.forecast[0][5]} %`;
+	p4El.textContent = "UV Index: ";
+	spanEl.textContent = data.forecast[0][6];
 
-    p4El.appendChild(spanEl);
-    forecastEl.appendChild(h3El);
-    forecastEl.appendChild(imgEl);
-    forecastEl.appendChild(p1El);
-    forecastEl.appendChild(p2El);
-    forecastEl.appendChild(p3El);
-    forecastEl.appendChild(p4El);
+	p4El.appendChild(spanEl);
+	forecastEl.appendChild(h2El);
+	forecastEl.appendChild(imgEl);
+	forecastEl.appendChild(p1El);
+	forecastEl.appendChild(p2El);
+	forecastEl.appendChild(p3El);
+	forecastEl.appendChild(p4El);
 
-    var forecast5DayEl = document.querySelector("#forecast5Day");
-    forecast5DayEl.innerHTML = '';
+	var forecast5DayEl = document.querySelector("#forecast5Day");
+	forecast5DayEl.innerHTML = '<h4>5-Day Forecast:</h4>';
 
-    for (let i = 1; i < data.forecast.length; i++) {
-        let div = document.createElement("div");
-        let h6 = document.createElement("h6");
-        let img = document.createElement("img");
-        let p1 = document.createElement("p");
-        let p2 = document.createElement("p");
-        let p3 = document.createElement("p");
+	for (let i = 1; i < data.forecast.length; i++) {
+		let div = document.createElement("div");
+		let h5 = document.createElement("h5");
+		let img = document.createElement("img");
+		let p1 = document.createElement("p");
+		let p2 = document.createElement("p");
+		let p3 = document.createElement("p");
 
-        div.setAttribute("class", "card");
-        img.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[i][1]}.png`);
-        img.setAttribute("alt", "forecast icon");
+		div.setAttribute("class", "card");
+		img.setAttribute("src", `http://openweathermap.org/img/w/${data.forecast[i][1]}.png`);
+		img.setAttribute("alt", "forecast icon");
 
-        h6.textContent = `${moment(data.forecast[i][0]).format("M/D/YYYY")}`;
-        p1.textContent = `Temp: ${convertKtoF(data.forecast[i][2])} °F`;
-        p2.textContent = `Wind: ${data.forecast[i][3]} MPH`;
-        p3.textContent = `Humidity: ${data.forecast[i][4]} %`;
+		h5.textContent = `${moment(data.forecast[i][0]).format("M/D/YYYY")}`;
+		p1.textContent = `Temp: ${convertKtoF(data.forecast[i][2])} °F`;
+		p2.textContent = `Wind: ${data.forecast[i][3]} MPH`;
+		p3.textContent = `Humidity: ${data.forecast[i][4]} %`;
 
-        div.appendChild(h6);
-        div.appendChild(img);
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
-        forecast5DayEl.appendChild(div);
-    }
+		div.appendChild(h5);
+		div.appendChild(img);
+		div.appendChild(p1);
+		div.appendChild(p2);
+		div.appendChild(p3);
+		forecast5DayEl.appendChild(div);
+	}
 
-    // update searches, 
-    if (searches.length) {
-        for (let i = 0;i <searches.length;i++) {
-            if(searches[i] == data.forecast[0][1].toLowerCase()) {
-                searches.splice(i, 1);
-                searches.push(data.forecast[0][1].toLowerCase());
-                renderSearches();
-                return;
-            } else if (i == 8) {
-                searches.splice(0, 1);
-                searches.push(data.forecast[0][1].toLowerCase());
-                renderSearches();
-            } else if (i == searches.length - 1) {
-                searches.push(data.forecast[0][1].toLowerCase());
-                renderSearches();
-            }
-        }
-    } else {
-        searches.push(data.forecast[0][1].toLowerCase());
-        renderSearches();
-    }
+	// update searches,
+	if (searches.length) {
+		for (let i = 0; i < searches.length; i++) {
+			if (searches[i] == data.forecast[0][1].toLowerCase()) {
+				searches.splice(i, 1);
+				searches.push(data.forecast[0][1].toLowerCase());
+				renderSearches();
+				return;
+			} else if (i == 8) {
+				searches.splice(0, 1);
+				searches.push(data.forecast[0][1].toLowerCase());
+				renderSearches();
+			} else if (i == searches.length - 1) {
+				searches.push(data.forecast[0][1].toLowerCase());
+				renderSearches();
+			}
+		}
+	} else {
+		searches.push(data.forecast[0][1].toLowerCase());
+		renderSearches();
+	}
 
 }
 
 function renderSearches() {
-    recentSearchesContainerEl.innerHTML = "";
+	recentSearchesContainerEl.innerHTML = "";
 
-    for (let i=0;i<searches.length;i++) {
-        let buttonEl =document.createElement("button");
-        buttonEl.setAttribute("data-city", searches[i].toLowerCase());
-        buttonEl.setAttribute("class", "recentSearchButton");
-        buttonEl.textContent = searches[i][0].toUpperCase() + searches[i].slice(1);
-        recentSearchesContainerEl.appendChild(buttonEl);
-    }
-    localStorage.setItem("searches", JSON.stringify(searches));
+	for (let i = 0; i < searches.length; i++) {
+		let buttonEl = document.createElement("button");
+		buttonEl.setAttribute("data-city", searches[i].toLowerCase());
+		buttonEl.setAttribute("class", "recentSearchButton");
+		buttonEl.textContent = searches[i][0].toUpperCase() + searches[i].slice(1);
+		recentSearchesContainerEl.appendChild(buttonEl);
+	}
+	localStorage.setItem("searches", JSON.stringify(searches));
 }
 
 function init() {
-    var storedSearches = JSON.parse(localStorage.getItem("searches"));
+	var storedSearches = JSON.parse(localStorage.getItem("searches"));
 
-    if (storedSearches !== null) {
-        searches = storedSearches;
-        renderSearches();
-        searchHelper(storedSearches[storedSearches.length - 1]);
-    }
+	if (storedSearches !== null) {
+		searches = storedSearches;
+		renderSearches();
+		searchHelper(storedSearches[storedSearches.length - 1]);
+	}
 }
 
 function storeForecast(forecast) {
-    localStorage.setItem(`forecast-${forecast.city.toLowerCase()}`, JSON.stringify(forecast));
+	localStorage.setItem(`forecast-${forecast.city.toLowerCase()}`, JSON.stringify(forecast));
 }
 
 function forecastFetch(city) {
-    var data = { city: null, date: null, forecast: [] };
-    let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=1b9775196b0ee2a34b1770325a87f87a`;
-    fetch(forecastURL)
-        .then(res => res.ok ? res.json() : Promise.reject(res))
-        .then(res => {
-            for (let i = 0; i <= 39; i += 8) {
-                if (i === 0) {
-                    data.city = res.city.name;
-                    data.date = res.list[i].dt_txt;
-                    data.forecast.push([res.list[i].dt_txt, res.city.name, res.list[i].weather[0].icon, res.list[i].main.temp, res.list[i].wind.speed, res.list[i].main.humidity]);
-                } else {
-                    data.forecast.push([res.list[i].dt_txt, res.list[i].weather[0].icon, res.list[i].main.temp, res.list[i].wind.speed, res.list[i].main.humidity]);
-                    if (i == 32) i--;
-                }
-            }
-            let uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${res.city.coord.lat}&lon=${res.city.coord.lat}&exclude=current,minutely,hourly,alerts&appid=1b9775196b0ee2a34b1770325a87f87a`;
-            return fetch(uviURL);
-        })
-        .then(res => res.ok ? res.json() : Promise.reject(res))
-        .then(res => {
-            data.forecast[0].push(res.daily[0].uvi);
-            storeForecast(data);
-            renderForecast(data);
-        })
-        .catch(err => {
-            console.log("err: ", err);
-            // if (err.status === 404)
-            //     return alert('404 Error: City not found please try again with a vaid city name. Ex. Seattle');
-            // alert(`${err.status} Error: ${err.message}`);
-            // if (res.status == 404) {
-            // } else {
-            //     return res.json();
-            // }
-        });
+	var data = { city: null, date: null, forecast: [] };
+	let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=1b9775196b0ee2a34b1770325a87f87a`;
+	fetch(forecastURL)
+		.then(res => res.ok ? res.json() : Promise.reject(res))
+		.then(res => {
+			for (let i = 0; i <= 39; i += 8) {
+				if (i === 0) {
+					data.city = res.city.name;
+					data.date = res.list[i].dt_txt;
+					data.forecast.push([res.list[i].dt_txt, res.city.name, res.list[i].weather[0].icon, res.list[i].main.temp, res.list[i].wind.speed, res.list[i].main.humidity]);
+				} else {
+					data.forecast.push([res.list[i].dt_txt, res.list[i].weather[0].icon, res.list[i].main.temp, res.list[i].wind.speed, res.list[i].main.humidity]);
+					if (i == 32) i--;
+				}
+			}
+			let uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${res.city.coord.lat}&lon=${res.city.coord.lat}&exclude=current,minutely,hourly,alerts&appid=1b9775196b0ee2a34b1770325a87f87a`;
+			return fetch(uviURL);
+		})
+		.then(res => res.ok ? res.json() : Promise.reject(res))
+		.then(res => {
+			data.forecast[0].push(res.daily[0].uvi);
+			storeForecast(data);
+			renderForecast(data);
+		})
+		.catch(err => {
+			// to do handle error
+			console.log("err: ", err);
+		});
 }
 
-
-// 1b9775196b0ee2a34b1770325a87f87a
-
 function searchHelper(city) {
-    var storedForecast = JSON.parse(localStorage.getItem(`forecast-${city}`));
-    if (storedForecast) {
-        if (moment(storedForecast.date).isSame(new Date(), 'day')) {
-            renderForecast(storedForecast);
-            return;
-        } else {
-            localStorage.removeItem(`forecast-${city}`);
-        }
-    }
-    forecastFetch(city);
+	var storedForecast = JSON.parse(localStorage.getItem(`forecast-${city}`));
+	if (storedForecast) {
+		if (moment(storedForecast.date).isSame(new Date(), 'day')) {
+			renderForecast(storedForecast);
+			return;
+		} else {
+			localStorage.removeItem(`forecast-${city}`);
+		}
+	}
+	forecastFetch(city);
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    var city = document.querySelector("#searchInput").value.trim().toLowerCase();
-    if (!city) {
-        // to do display error message
-    } else {
-        searchHelper(city);
-    }
+	var city = document.querySelector("#searchInput").value.trim().toLowerCase();
+	if (!city) {
+		// to do display error message
+	} else {
+		searchHelper(city);
+	}
 }
 
 searchFormEl.addEventListener("submit", handleSubmit);
 
-recentSearchesContainerEl.addEventListener("click", function(event) {
-    var el = event.target;
-    console.log("click hit")
-    if (el.matches("button")) {
-        var city = el.getAttribute("data-city");
-        searchHelper(city);
-    }
+recentSearchesContainerEl.addEventListener("click", function (event) {
+	var el = event.target;
+	if (el.matches("button")) {
+		var city = el.getAttribute("data-city");
+		searchHelper(city);
+	}
 });
 
 init();
